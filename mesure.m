72fa320320866cbe4e80a3411a5da2ebd1cc9787
axnj2 <52408308+axnj2@users.noreply.github.com>
2025-03-02 17:49:25 +0100

    added openinout and save the data

--- mesure.m
+++ mesure.m
@@ -4,11 +4,16 @@ clc;
 
 duration = 20; % durée de l'expérience
 fs = 1000; % fréquence d'échantillonage
+
+openinout; %Permet l'accès aux ports du calculateur analogique.
 [time, data] = acqui2(fs,duration);
+closeinout %Permet de retirer l'accès aux ports du calculateur analogique.
 % time est un vecteur colonne de fs*duration lignes
 % data est une matrice de 8 colonnes et fs*duration lignes
 
 input_channel = 1;
 output_channel = 2;
-input = data(1:end,input_channel);
-output = data(1:end,output_channel);
+input = data(:, input_channel);
+output = data(:, output_channel);
+
+save("mesure_XXXX.mat", "time", "input", "output");
