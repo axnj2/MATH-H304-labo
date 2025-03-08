59a0e16a4ef08affd62c91dde281ce17a0d9dbfb
axnj2 <52408308+axnj2@users.noreply.github.com>
2025-03-08 14:22:51 +0100

    added a plot

--- labo2_reg_prop_simu.m
+++ labo2_reg_prop_simu.m
@@ -4,6 +4,9 @@ clear all;
 close all;
 clc;
 
+%%% performance criteria parameters
+% number of points for the average
+N = 100;
 
 %%%%%%% parameters for the simulink model %%%%%%%
 % system model parameters
@@ -15,7 +18,7 @@ y0 = 0;
 u0 = 0.32;
 
 % gain du régulateur proportionnel
-K = 3.5;
+K = 0.25;
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -34,8 +37,8 @@ if from_real_data
     load(data_to_load,  "time", "input", "output", "Fs");
     perturbation_step_amplitude = 0;
 else
-    Fs = 10000;
-    duration = 10;
+    Fs = 1000;
+    duration = 30;
     % time vector
     time = 0:1/Fs:duration;
     time = time';
@@ -72,6 +75,37 @@ mdl_name = "simulink/simulink_s2_22b_22c_2020a_03.slx";
 % run simulink model with the time vector
 out = sim(mdl_name, time); 
 out.yout.plot
+%%% get the results from the simulation
+commande_signal = out.yout.getElement(3).Values.Data;
+output_signal = out.yout.getElement(4).Values.Data;
+reference_signal = out.yout.getElement(2).Values.Data;
+real_output_signal = out.yout.getElement(1).Values.Data;
+
+%%% Calculate the performance criteria
+% static error
+static_error = abs(mean(output_signal(end-N:end)) - mean(reference_signal(end-N:end)))
+% TODO facteur d'amortissement
+
+%%%%%%%%%%%%% Ploting the results %%%%%%%%%%%%%%%%
+%%% plot the results including:
+% le signal de commande u(t)
+% le signal de sortie y(t)
+% le signal de consigne r(t)
+
+figure;
+hold on;
+plot(time, commande_signal, 'DisplayName', 'commande u(t)');
+plot(time, output_signal, 'DisplayName', 'sortie y(t)');
+plot(time, reference_signal, 'DisplayName', 'consigne r(t)');
+if from_real_data
+    plot(time, real_output_signal, 'DisplayName', 'sortie réelle y(t)');
+end
+hold off;
+legend;
+xlabel('temps (s)');
+ylabel('amplitude (V)');
+
+
 
 
 
