a8b491ec54ad32851d26092a5d01e63684b3bffa
cketuveufrr <147418448+cketuveufrr@users.noreply.github.com>
2025-05-05 13:38:29 +0200

    Update script_param_s7.m

--- labo7/script_param_s7.m
+++ labo7/script_param_s7.m
@@ -1,28 +1,31 @@
-%%% Le but de ce script est de définir les valeurs des paramètres utilisés
-%%% dans le fichier Simulink "SimuBFContinuDiscret" <- situé dans le dossier simulink
+Simulink.sdi.clear;
 
+%%% Le but de ce script est de définir les valeurs des paramètres utilisés
+%%% dans le fichier Simulink "SimuBFContinuDiscret"
 
 %%% Système
-A0 = ;      %Gain statique
-tau1 = ;    %Constante de temps 1
-tau2 = ;    %Constante de temps 2 (0 si 1er ordre)
-u0 = ;      %Point de fonctionnement : grandeur réglante
-y0 = ;      %Point de fonctionnement : grandeur réglée
-fs = ;      %Fréquence d'échantillonnage
-echelon = ; %Amplitude de l'échelon de consigne
+A0 = 5.35;     %Gain statique
+T = 0.44;    %Constante de temps 1
+u0 = 0;      %Point de fonctionnement : grandeur réglante
+y0 = 0;      %Point de fonctionnement : grandeur réglée
+fs = 0.5;      %Fréquence d'échantillonnage
+echelon = 1; %Amplitude de l'échelon de consigne
 
 % Si votre système n'est pas intégrateur, décommentez la ligne suivante
 % den = [tau1*tau2 tau1+tau2 1];
 
 % Si votre système est intégrateur, décommentez la ligne suivante
-den = [tau1*tau2 tau1+tau2 1 0];
+den = [T 1 0];
 
 
 %%% Contrôleur
-kp = ;     %Gain
+kp = 0.22;      %Gain
 
 
 %%% Filtre anti-repli
-zeta = ;   %Facteur d'amortissement
-T = ;      %Position de la paire de pôle
-wn = ;     %pulsation naturelle
+zeta = 0.7;   %Facteur d'amortissement
+T = 0.01;      %Position de la paire de pôle
+wn =  100;     %pulsation naturelle
+
+out = sim("SimuBFContinuDiscret_2020a.slx")
+out.yout.plot
